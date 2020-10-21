2bc12381314c67bf6b843fc3135eb55ce6cde88e
Victor <vico.dusa@gmail.com>
2020-10-21 10:08:22 -0500

    cc

--- main.cpp
+++ main.cpp
@@ -1,13 +1,14 @@
 #include <chrono>
 #include <sstream>
-#include <iostream>
+#include <cstdio>
 #include "MatchingEngine.h"
 
 int main() {
     auto* m = new MatchingEngine();
     m->addSymbol(1);
     auto start = std::chrono::high_resolution_clock::now();
-    for (int i = 0; i < 2000000; i+=10) {
+    int N = 2000000;
+    for (int i = 0; i < N; i+=10) {
         m->placeOrder(i+0,2, 1, 8, 5,false);
         m->placeOrder(i+1,2, 1, 5, 5,false);
         m->placeOrder(i+2,1, 1, 8, 4,true);
@@ -21,6 +22,6 @@ int main() {
     }
     auto stop = std::chrono::high_resolution_clock::now();
     std::chrono::duration<double> diff = stop-start;
-    std::cout << diff.count() << std::endl;
+    printf("Simulated %d Trades in %dms\n", N, (int)(diff.count()*1000));
     return 0;
 }
